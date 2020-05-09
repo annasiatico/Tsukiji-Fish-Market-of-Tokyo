2fc199ae5caca89fa7426c18c17f97c5c43c96b4
Ann Asiatico <annasiatico@gmail.com>
2020-05-09 00:44:56 -0500

    updated file

--- predict.py
+++ predict.py
@@ -2,10 +2,7 @@ from joblib import load
 from preprocess import prep_data
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
 
 def predict_from_csv(path_to_csv):
 
